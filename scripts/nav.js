47ebec643b18e9b40526d2d464ef3490dc83c0b4
ConeyNicollete-FS <naclemons@student.fullsail.edu>
2021-09-19 21:15:21 -0700

    Adding state changes to links and buttons for hover and active

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,6 +1,33 @@
 class Navigation {
     constructor() {
         document.querySelector("#navbarToggleExternalContent").addEventListener("click", (e)=>this.toggle(e));
+
+        const sections = document.querySelectorAll("section");
+        const navLi = document.querySelectorAll("header nav li a");
+        window.onscroll = () => {
+        let current = "";
+
+            sections.forEach((section) => {
+                const sectionTop = section.offsetTop;
+                if(window.matchMedia("(min-width: 300px)")) {
+                    if (pageYOffset >= sectionTop - 755) {
+                        current = section.getAttribute("id");
+                    }
+                }
+                else if(window.matchMedia("(min-width: 996px)")) {
+                    if (pageYOffset >= sectionTop - 235) {
+                        current = section.getAttribute("id");
+                    }
+                }
+            });
+
+            navLi.forEach((li) => {
+                li.classList.remove("current");
+                if (li.classList.contains(current)) {
+                li.classList.add("current");
+                }
+            });
+        };
     }
 
     toggle(e) {
